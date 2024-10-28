e392904aac524a5802bf5249ba197880e9291fbe
takaki <takaki55730317@gmail.com>
2024-10-28 23:12:31 +0900

    正常な時のterraformに戻し

--- infrastructure/modules/database/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_endpoint" {
-  description = "The connection endpoint for the RDS instance"
-  value       = aws_db_instance.mysql.endpoint
-}
-
-output "db_instance_id" {
-  description = "The ID of the RDS instance"
-  value       = aws_db_instance.mysql.id
-}
